bff634c65bd69bfa02bfc360791e4783828383b0
mike-petrov <mis5116@yandex.ru>
2019-03-24 00:33:49 +0300

    new block Tinder 2.0

---
+++ src/Tinder/arrayCard.js
@@ -0,0 +1,21 @@
+export var objs = [{
+        photo: "https://i.ibb.co/sHNmPt5/q2.jpg",
+        name: "Кофта",
+        type: 1
+    },
+    {
+        photo: "https://i.ibb.co/sHNmPt5/q2.jpg",
+        name: "Майка",
+        type: 2
+    },
+    {
+        photo: "https://i.ibb.co/sHNmPt5/q2.jpg",
+        name: "Кофта",
+        type: 3
+    },
+    {
+        photo: "https://i.ibb.co/sHNmPt5/q2.jpg",
+        name: "Сандалии",
+        type: 3
+    }
+]
