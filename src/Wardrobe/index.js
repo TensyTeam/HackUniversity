0b5a9189ba70c27bf58ed3f2101126953b847a77
mike-petrov <mis5116@yandex.ru>
2019-03-24 07:40:22 +0300

    new wardrobe

--- src/Wardrobe/index.js
+++ src/Wardrobe/index.js
@@ -6,45 +6,9 @@ import './style.css'
 export default class Wardrobe extends React.Component {
 	render() {
 		return (
-			<div id="wardrobe">123123123123123123<br />
-			123<br />
-			123<br />
-			v<br />
-			123123<br />
-			123<br /><br />
-
-			123<br /><br /><br /><br /><br /><br />
-			123
-			123<br /><br />
-			123
-			123<br /><br />
-			123
-			123<br /><br /><br /><br /><br /><br />
-
-			123
-			123
-			123<br /><br /><br /><br /><br /><br />
-			123
-			123
-			123
-			123
-			123<br /><br />
-			123
-			123<br />
-			123<br />
-
-			123
-			123<br /><br /><br /><br /><br /><br /><br />
-			123
-			123<br /><br /><br /><br /><br />
-			<br />
-			<br />69alalalallalala
-			<br />567
-			<br />
-			<br />456
-			<br />
-			123
-			123</div>
+			<div id="wardrobe">
+				{this.props.children}
+			</div>
 		)
 	}
 }
