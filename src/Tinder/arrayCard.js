2f2c0719dd65e37253e40de926ba0b84f208a07f
mike-petrov <mis5116@yandex.ru>
2019-03-24 07:41:23 +0300

    new data card

--- src/Tinder/arrayCard.js
+++ src/Tinder/arrayCard.js
@@ -3,15 +3,19 @@ export var objs = [{
         name: "Кофта",
     },
     {
-        photo: "https://i.ibb.co/sHNmPt5/q2.jpg",
-        name: "Майка",
+        photo: "https://i.ibb.co/54BSy1v/photo-2019-03-24-06-34-27.jpg",
+        name: "Кофта",
+    },
+    {
+        photo: "https://i.ibb.co/C73Bnx0/photo-2019-03-24-06-34-42.jpg",
+        name: "Футболка",
+    },
+    {
+        photo: "https://i.ibb.co/JmDkyGs/photo-2019-03-24-06-34-19.jpg",
+        name: "Куртка",
     },
     {
         photo: "https://i.ibb.co/sHNmPt5/q2.jpg",
         name: "Кофта",
-    },
-    {
-        photo: "https://i.ibb.co/sHNmPt5/q2.jpg",
-        name: "Сандалии",
     }
 ]
